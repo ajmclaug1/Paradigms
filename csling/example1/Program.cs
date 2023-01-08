12fb0b6df8338e95608e8fe02bace037c3311510
ajmclaug1 <ajmclaug1@gmail.com>
2023-01-08 00:49:59 +0000

    moved files and renamed others

---
+++ csling/example1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Csling
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            IEnumerable<string> cities = new[] {"Ghent", "London", "Las Vegas", "Rivia"};
+
+           
+        }
+
+
+    }
+}
+
+namespace Extensions
+{
+    
+}
---
+++ csling/example2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace csling;
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = new DateTime(2002, 8, 9);
+
+            int daysTillEndOfMonth = date.DaysTillEndOfMonth();
+
+            Console.WriteLine(daysTillEndOfMonth);
+        }
+    }
+
+    public static class DateUtilities
+    {
+        public static int DaysTillEndOfMonth(this DateTime date)
+        {
+            return DateTime.DaysInMonth(date.Year, date.Month) - date.Day;
+
+        }
+    }
+
+
