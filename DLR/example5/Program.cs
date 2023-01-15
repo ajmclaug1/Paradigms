3521a92e7f5b934b66a1910528d5e9261ed00140
ajmclaug1 <ajmclaug1@gmail.com>
2023-01-15 22:25:55 +0000

    Changes

--- DLR/example5/Program.cs
+++ DLR/example5/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Diagnostics;
+using System.Dynamic;
+
+namespace csdlr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic expando = new ExpandoObject();
+            expando.Name = "Anthony";
+            expando.Speak = new Action(() => Console.WriteLine(expando.Name));
+        }
+    }
+}
